13793f712f49d9cacc8a62078040dfcbed920af7
Éder Luís Britto Garcia <eder.projetos.dev@gmail.com>
2023-01-14 21:25:28 -0300

    Criada a segunda tela nomeada como janela2

--- main.py
+++ main.py
@@ -4,46 +4,82 @@ from imdb import diretores_por_letra
 from imdb import filmes_por_artista
 from imdb import filmes_por_diretor
 
-alfabeto = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".split(',')
+def criar_janela1():
 
-psg.theme('DarkGray13')
+    alfabeto = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".split(',')
 
-layout = [
-    [psg.Text('ATORES (A-Z)', size=(24, 0), font='Lucida', justification='center'),
-     psg.Text('DIRETORES (A-Z)', size=(24, 0), font='Lucida', justification='center')],
-    [psg.Combo(alfabeto, default_value='  ESCOLHA UMA LETRA', key='alfa_ator', readonly=True, size=(30, 6), enable_events=True),
-     psg.Combo(alfabeto, default_value='  ESCOLHA UMA LETRA', key='alfa_diretor', readonly=True, size=(30, 6), enable_events=True)],
-    [psg.Listbox(values=[], select_mode='single', key='atores', size=(30, 6), enable_events=True),
-     psg.Listbox(values=[], select_mode='single', key='diretores', size=(30, 6), enable_events=True)],
-    [psg.Text('FILMES', size=(50, 0), font='Lucida', justification='center')],
-    [psg.Listbox(values=[], select_mode='single', key='filmes', size=(64, 6))]
-]
+    psg.theme('DarkGray13')
 
-janela = psg.Window('IMDB TOP 1000 FILMES', layout)
-
-while True:
-    evento, valores = janela.read()
-    if evento == psg.WIN_CLOSED or evento == "Cancelar":
-        break
+    layout = [
+        [psg.Text('ATORES (A-Z)', size=(24, 0), font='Lucida', justification='center'),
+         psg.Text('DIRETORES (A-Z)', size=(24, 0), font='Lucida', justification='center')],
+        [psg.Combo(alfabeto, default_value='  ESCOLHA UMA LETRA', key='alfa_ator', readonly=True, size=(30, 6), enable_events=True),
+         psg.Combo(alfabeto, default_value='  ESCOLHA UMA LETRA', key='alfa_diretor', readonly=True, size=(30, 6), enable_events=True)],
+        [psg.Listbox(values=[], select_mode='single', key='atores', size=(30, 6), enable_events=True),
+         psg.Listbox(values=[], select_mode='single', key='diretores', size=(30, 6), enable_events=True)],
+        [psg.Text('FILMES', size=(50, 0), font='Lucida', justification='center')],
+        [psg.Listbox(values=[], select_mode='single', key='filmes', size=(64, 6), enable_events=True)]]
     
-    if len(valores['alfa_ator']) == 1:
-        letra = valores['alfa_ator']
-        count_atores, atores = artistas_por_letra(letra)
-        janela["atores"].update(values=atores)
-                 
-    if len(valores['alfa_diretor']) == 1:
-        letra = valores['alfa_diretor']
-        count_diretores, diretores = diretores_por_letra(letra)
-        janela["diretores"].update(values=diretores)
+    return psg.Window('IMDB TOP 1000 FILMES', layout, finalize=True)
 
-    if evento == 'atores':
-        selection = valores[evento]
-        contagem, filmes_artista = filmes_por_artista(''.join(selection)) 
-        janela["filmes"].update(values=filmes_artista)
 
-    if evento == 'diretores':
-        selection = valores[evento]
-        contagem, filmes_diretor = filmes_por_diretor(''.join(selection))
-        janela["filmes"].update(values=filmes_diretor)        
+def criar_janela2():
+    psg.theme('DarkGray13')
+    layout = [[psg.Text('NOME DO FILME', key='nome_do_filme', size=(24, 0), font='Lucida', justification='center')],
+              [psg.Output(size=(260, 10), key='dados_do_filme')],
+              [psg.Button('VOLTAR')]]
+    return psg.Window('IMDB TOP 1000 FILMES', layout, size=(300, 300), element_justification='c', finalize=True)
 
-janela.close()
+
+def main():
+
+    janela1, janela2 = criar_janela1(), criar_janela2()
+    janela2.hide()
+    
+    while True:
+
+        janela, evento, valores = psg.read_all_windows()
+        
+        if evento == psg.WIN_CLOSED or evento == 'Finalizar':
+            janela.close()
+            if janela == janela2:
+                janela2 = None
+            elif janela == janela1:
+                janela1 = None
+            break
+        
+        if evento == 'alfa_ator':
+            if len(valores['alfa_ator']) == 1:
+                letra = valores['alfa_ator']
+                count_atores, atores = artistas_por_letra(letra)
+                janela["atores"].update(values=atores)
+                
+        if evento == 'alfa_diretor':             
+            if len(valores['alfa_diretor']) == 1:
+                letra = valores['alfa_diretor']
+                count_diretores, diretores = diretores_por_letra(letra)
+                janela["diretores"].update(values=diretores)
+
+        if evento == 'atores':
+            selecionado = valores[evento]
+            contagem, filmes_artista = filmes_por_artista(''.join(selecionado)) 
+            janela["filmes"].update(values=filmes_artista)
+
+        if evento == 'diretores':
+            selecionado = valores[evento]
+            contagem, filmes_diretor = filmes_por_diretor(''.join(selecionado))
+            janela["filmes"].update(values=filmes_diretor)
+
+        if evento == 'filmes':
+            selecionado = valores[evento]
+            janela1.hide()
+            janela2.un_hide()            
+            janela2["nome_do_filme"].update(''.join(selecionado))
+            janela2["dados_do_filme"].update(''.join(selecionado))            
+            
+        if janela == janela2 and evento == 'VOLTAR':
+            janela2.hide()
+            janela1.un_hide()
+
+if __name__ == '__main__':
+    main()
